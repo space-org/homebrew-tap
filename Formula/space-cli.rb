7cf77c28b3eaa306028ddf7c12af756b303bbf0d
Till Friebe <till@friebe.dev>
2026-04-19 14:17:28 +0200

    space-cli v0.1.0
    macOS ARM64, Linux ARM64, Linux x86_64.
    Intel macOS is unavailable for this release (GitHub Actions Intel-runner
    queue issues); will return in a later version.

--- Formula/space-cli.rb
+++ Formula/space-cli.rb
@@ -7,22 +7,18 @@ class SpaceCli < Formula
   on_macos do
     on_arm do
       url "https://github.com/space-org/space-cli/releases/download/v0.1.0/space-cli-macos-arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
-    end
-    on_intel do
-      url "https://github.com/space-org/space-cli/releases/download/v0.1.0/space-cli-macos-x86_64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "e771b5368a4ea71355411cf21c36923d4cebd55940d2952511050781ac553a36"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/space-org/space-cli/releases/download/v0.1.0/space-cli-linux-arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "bf13c17d8a6f4f05a066b62061fd898852dcdb3074632cbbf984a366a188c390"
     end
     on_intel do
       url "https://github.com/space-org/space-cli/releases/download/v0.1.0/space-cli-linux-x86_64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "7c1a810db892e8535f544521332858e8fa88847fd4eb86ae42bfcb7dd5d1bf37"
     end
   end
 
